37ce0ef12b2f4eff2d7d458df4968ae37a717ccf
Kaustubh <kaustubhbajirao@gmail.com>
2023-12-30 00:34:43 +0530

    First Commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+D:\Selenium\Opencart
+mvn test
---
+++ src/test/java/pageObjects/MyAccountPage.java
@@ -0,0 +1,32 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MyAccountPage extends BasePage {
+
+	public MyAccountPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(xpath = "//h2[text()='My Account']") // MyAccount Page heading
+	WebElement msgHeading;
+	
+	@FindBy(xpath="//*[@id='column-right']/div/a[13]")
+	WebElement lnklogout;
+
+	public boolean isMyAccountPageExists()   // MyAccount Page heading display status
+	{
+		try {
+			return (msgHeading.isDisplayed());
+		} catch (Exception e) {
+			return (false);
+		}
+	}
+	
+	public void clickLogout() {
+		lnklogout.click();
+	}
+
+}
